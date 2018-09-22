ad6706e5c58cd993d4f82c5ae5456fb97467d406
SerafimPoch <serpochapinsky@gmail.com>
2018-09-22 17:38:50 +0300

    add loginUser reducer tests

--- src/store/reducers/loginUser.js
+++ src/store/reducers/loginUser.js
@@ -1,13 +1,13 @@
 import { LOGIN_USER_SUCCESS, CLEAN_LOGIN_USER } from "../actions/actionTypes";
 
-const init = [];
+const init = {};
 
 export default (state = init, { type, payload }) => {
   switch (type) {
     case LOGIN_USER_SUCCESS:
       return payload;
     case CLEAN_LOGIN_USER:
-      return [];
+      return {};
     default:
       return state;
   }
---
+++ src/tests/reducers/loginUser.test.js
@@ -0,0 +1,36 @@
+import reducer from "../../store/reducers/loginUser";
+import * as types from "../../store/actions/actionTypes";
+
+describe("loginUser reducer", () => {
+  it("should return the initial state", () => {
+    expect(reducer({}, {})).toEqual({});
+  });
+
+  it("should handle LOGIN_USER_SUCCESS", () => {
+    const loginUserData = {
+      success: true,
+      token:
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1Mzc2NDQyMDksInVzZXJuYW1lIjoiRGFya2VubWFnIiwidXVpZCI6IjdlZGU4MTk1LWJkN2MtNDU1Mi04YjFlLTE2MzkzZmE3YjMxMCIsImRldmljZV91dWlkIjoiMDllNzZhY2ItMzRkNS00Y2RkLTgyNDYtZTExMzk1NDNmMzdlIiwic3RvcmVfaWQiOjE1fQ.ckNyuYdsPOdmCxz9oG-fxLSZikzlzx3Ek-e6W2bJs2c"
+    };
+    expect(
+      reducer(
+        {},
+        {
+          type: types.LOGIN_USER_SUCCESS,
+          payload: loginUserData
+        }
+      )
+    ).toEqual(loginUserData);
+  });
+
+  it("should handle CLEAN_LOGIN_USER", () => {
+    expect(
+      reducer(
+        {},
+        {
+          type: types.CLEAN_LOGIN_USER
+        }
+      )
+    ).toEqual({});
+  });
+});
